37305e08c6604b580cf0545d34b7ff2c05a3ee61
theviper1001 <divyakrgupta1001@hotmail.com>
2022-08-26 00:12:18 +0530

    Update search.java

--- src/test/java/com/google/himarsha/search.java
+++ src/test/java/com/google/himarsha/search.java
@@ -27,8 +27,8 @@ public class search {
 		searchbox.sendKeys("Himarsha Venkatswamy Bikini");
 		searchbox.sendKeys(Keys.ENTER);
 		 
-		WebElement image= driver.findElement(By.linkText("Images"));
-		image.click();
+		WebElement images= driver.findElement(By.linkText("Images"));
+		images.click();
 		
 		Assert.assertEquals(driver.getTitle(),"Himarsha Venkatsamy Bikini - Google Search");
 		Thread.sleep(500);
